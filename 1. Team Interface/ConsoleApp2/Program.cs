ef0848a2c7418ad29d433305d2d8d84d9e88236c
RahimliZakir <zakirer@code.edu.az>
2024-05-01 19:13:55 +0400

    TestTasks

---
+++ 1. Team Interface/ConsoleApp2/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public class Team
+    {
+        public string teamName { get; set; }
+        public int noOfPlayers { get; set; }
+
+        public Team(string teamName, int noOfPlayers)
+        {
+            this.teamName = teamName;
+            this.noOfPlayers = noOfPlayers;
+        }
+
+        public void AddPlayer(int count)
+        {
+            this.noOfPlayers += count;
+        }
+
+        public bool RemovePlayer(int count)
+        {
+            if (count > this.noOfPlayers)
+            {
+                return false;
+            }
+            else
+            {
+                this.noOfPlayers -= count;
+                return true;
+            }
+
+        }
+    }
+
+    public class Subteam : Team
+    {
+        public Subteam(string teamName, int noOfPlayers)
+            : base(teamName, noOfPlayers) { }
+
+        public void ChangeTeamName(string name)
+        {
+            this.teamName = name;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!typeof(Subteam).IsSubclassOf(typeof(Team)))
+            {
+                throw new Exception("Subteam class should inherit from Team class");
+            }
+
+            String str = Console.ReadLine();
+            String[] strArr = str.Split();
+            string initialName = strArr[0];
+            int count = Convert.ToInt32(strArr[1]);
+            Subteam teamObj = new Subteam(initialName, count);
+            Console.WriteLine("Team " + teamObj.teamName + " created");
+
+            str = Console.ReadLine();
+            count = Convert.ToInt32(str);
+            Console.WriteLine("Current number of players in team " + teamObj.teamName + " is " + teamObj.noOfPlayers);
+            teamObj.AddPlayer(count);
+            Console.WriteLine("New number of players in team " + teamObj.teamName + " is " + teamObj.noOfPlayers);
+
+
+            str = Console.ReadLine();
+            count = Convert.ToInt32(str);
+            Console.WriteLine("Current number of players in team " + teamObj.teamName + " is " + teamObj.noOfPlayers);
+            var res = teamObj.RemovePlayer(count);
+            if (res)
+            {
+                Console.WriteLine("New number of players in team " + teamObj.teamName + " is " + teamObj.noOfPlayers);
+            }
+            else
+            {
+                Console.WriteLine("Number of players in team " + teamObj.teamName + " remains same");
+            }
+
+            str = Console.ReadLine();
+            teamObj.ChangeTeamName(str);
+            Console.WriteLine("Team name of team " + initialName + " changed to " + teamObj.teamName);
+        }
+
+    }
+}
---
+++ 2. Company Employee Management/ConsoleApp1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Company { get; set; }
+
+        public void Get(int a)
+        {
+            this.Age += a;
+        }
+    }
+
+
+    class Program
+    {
+        public static Dictionary<string, int> AverageAgeForEachCompany(List<Employee> employees)
+        {
+            return employees.GroupBy(g => g.Company).Select(g => new
+            {
+                Company = g.Key,
+                Age = Convert.ToInt32(Math.Round(g.Average(g => g.Age)))
+            }).ToDictionary(g => g.Company, g => g.Age);
+        }
+
+        public static Dictionary<string, int> CountOfEmployeesForEachCompany(List<Employee> employees)
+        {
+            return employees.GroupBy(g => g.Company).Select(g => new
+            {
+                Company = g.Key,
+                Employees = g.Count()
+            }).ToDictionary(g => g.Company, g => g.Employees);
+        }
+
+        public static Dictionary<string, Employee> OldestAgeForEachCompany(List<Employee> employees)
+        {
+            return employees.GroupBy(g => g.Company).Select(g => new
+            {
+                Company = g.Key,
+                Oldest = g.OrderByDescending(_ => _.Age).FirstOrDefault()
+            }).ToDictionary(g => g.Company, g => g.Oldest);
+        }
+
+        static void Main(string[] args)
+        {
+            int countOfEmployees = int.Parse(Console.ReadLine());
+
+            List<Employee> employees = new();
+
+            for (int i = 0; i < countOfEmployees; i++)
+            {
+                string str = Console.ReadLine();
+                string[] strArr = str.Split(' ');
+                employees.Add(new Employee
+                {
+                    FirstName = strArr[0],
+                    LastName = strArr[1],
+                    Company = strArr[2],
+                    Age = int.Parse(strArr[3])
+                });
+            }
+
+            //foreach (var emp in AverageAgeForEachCompany(employees))
+            //{
+            //    Console.WriteLine($"The average age for company {emp.Key} is {emp.Value}");
+            //}
+
+            //foreach (var emp in CountOfEmployeesForEachCompany(employees))
+            //{
+            //    Console.WriteLine($"The count of employees for company {emp.Key} is {emp.Value}");
+            //}
+
+            foreach (var emp in OldestAgeForEachCompany(employees))
+            {
+                Console.WriteLine($"The oldest employee of company {emp.Key} is {emp.Value.FirstName} {emp.Value.LastName} having age {emp.Value.Age}");
+            }
+        }
+    }
+}
